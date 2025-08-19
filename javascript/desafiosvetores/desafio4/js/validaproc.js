e9bccfe202570b70ace88c18bc297f8ea4bdc9bf
emanueloliveirasenai <emanuel_oliveira151@estudante.sesisenai.org.br>
2025-08-19 20:13:56 -0300

    Desafios de vetores

--- javascript/desafiosvetores/desafio4/js/validaproc.js
+++ javascript/desafiosvetores/desafio4/js/validaproc.js
@@ -1,20 +1,21 @@
-//Faça um programa para ler as notas obtidas pelos alunos, e depois exibir um relatório das notas iguais ou superiores a 7,5.
+/*A empresa “ORDEM DECRESCENTE” necessita de uma solução que alimente um 
+vetor com 4 valores numéricos inteiros. Como resultado deseja-se que imprima os 
+valores na ordem inversa com que foram armazenadas, ou seja, do valor localizado 
+na última posição para o valor localizado na primeira posição do vetor*/
+
 function validaProc(){
 
     let vetor = [];
     let i;
 
-    for(i = 0; i < 3; i++){
-        vetor[i] = parseFloat(prompt("Insira a " + (i+1) + "ª nota"));
+    for(i = 0; i < 4; i++){
+        vetor[i] = parseInt(prompt("Insira o " + (i+1) + "º Valor"))
+        console.log("O número inserido na posição " + (i+1) + " do vetor é: " + vetor[i])
     }
 
-    console.log("\nOs alunos com notas iguais ou superiores a 7,5 são:\n");
-
-    for(i = 0; i < 3; i++){
-        if(vetor[i] >= 7.5){
-            console.log("\nAluno " + (i+1) + " com nota " + vetor[i]);
-        }
+    for(i = 3; i >= 0; i--){
+        console.log("O número na posição " + (i+1) + " do vetor é " + vetor[i])
     }
-
+    
     return false;
 }
