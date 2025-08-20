e58de584bc045810c89b331c6565f91baf0eee3d
emanueloliveirasenai <emanuel_oliveira151@estudante.sesisenai.org.br>
2025-08-19 21:50:14 -0300

    Atividades de matriz

---
+++ javascript/matriz/atividade3/js/validaproc.js
@@ -0,0 +1,9 @@
+/*Crie uma matriz (2x2) onde o valor de cada elemento dentro da matriz é a soma
+dos índices da sua posição*/
+function validaProc(){
+
+    let matriz = [];
+    let linha = 2;
+    let coluna = 2;
+    
+}
